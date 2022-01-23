531699d288343f042dbd4d4e7e81955281bee3ca
Jay Zisch <jz709u@gmail.com>
2022-01-23 22:23:16 +0900

    dependencies

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "MyLibrary2",
+    platforms: [
+        .iOS(.v15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -12,6 +15,8 @@ let package = Package(
             targets: ["MyLibrary2"]),
     ],
     dependencies: [
+        .package(name: "RxSwift", url: "https://github.com/ReactiveX/RxSwift", from: .init(6, 5, 0)),
+        .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.1"))
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
@@ -20,7 +25,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MyLibrary2",
-            dependencies: []),
+            dependencies: ["RxSwift", "SnapKit",
+                           .product(name: "RxCocoa", package: "RxSwift", condition: nil)]),
         .testTarget(
             name: "MyLibrary2Tests",
             dependencies: ["MyLibrary2"]),
--- Sources/MyLibrary2/MyLibrary2.swift
+++ Sources/MyLibrary2/MyLibrary2.swift
@@ -1,6 +1,57 @@
+import UIKit
+import RxSwift
+import SnapKit
+import RxRelay
+import RxCocoa
+
 public struct MyLibrary2 {
-    public private(set) var text = "Hello, World!"
+    public private(set) var text = "Hello, World! 111"
 
     public init() {
     }
+}
+
+public class MyLibraryController2: UIViewController {
+    let viewModel = MyLibraryControllerViewModel()
+    let myLabel = UILabel()
+    let disposeBag = DisposeBag()
+    @IBOutlet weak var button: UIButton!
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.addSubview(myLabel)
+        myLabel.snp.makeConstraints { make in
+            make.bottom.equalToSuperview()
+        }
+        myLabel.text = MyLibrary2().text
+        
+        button.rx.tap
+            .bind(to: viewModel.tappedButton)
+            .disposed(by: disposeBag)
+        
+        viewModel.myLabelText
+            .bind(to: myLabel.rx.text)
+            .disposed(by: disposeBag)
+    }
+}
+
+public class MyLibraryControllerViewModel {
+    let tappedButton: PublishRelay<Void> = .init()
+    let myLabelText: BehaviorRelay<String> = .init(value: "")
+    let disposeBag = DisposeBag()
+    init() {
+        tappedButton.map {
+            "Hello Tap"
+        }.bind(to: myLabelText)
+            .disposed(by: disposeBag)
+    }
+    
+}
+
+public class MyLibraryStoryboard {
+    public static let storyboard: UIStoryboard! = {
+        UIStoryboard(name: "Storyboard", bundle: Bundle.module)
+    }()
+    public static let storyboardInitialVC: UIViewController = {
+        storyboard.instantiateInitialViewController()!
+    }()
 }
